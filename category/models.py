5132a3dabb53408f0dfad9e8fda6ea8e9452b066
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-05 01:25:02 +0300

    Linking category to image models

--- category/models.py
+++ category/models.py
@@ -5,3 +5,7 @@ class Category(models.Model):
     category_name = models.CharField(max_length=100)
     category_slug = models.CharField(max_length=100)
     category_desc = models.CharField(max_length=1000)
+
+    #Representation in admin dashboard or views
+    def __str__(self) -> str:
+        return self.category_name
--- image/models.py
+++ image/models.py
@@ -1,5 +1,6 @@
 from tkinter import image_names
 from django.db import models
+from category.models import Category
 
 # Create your models here.
 
@@ -10,6 +11,7 @@ class Image(models.Model):
     image_desc =models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now= True)
     
+    category= models.ForeignKey(Category, on_delete= models.CASCADE)
     #Representation of what will be represented in view or admin dashboard
     
     def __str__(self) -> str:

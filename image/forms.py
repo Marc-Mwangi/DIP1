05f7d7773894947a4a0053a90c723febd7d28429
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-13 04:59:39 +0300

    Linking category model to Image forms

--- image/forms.py
+++ image/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from category.models import Category
 
 class ImageForm(forms.Form):
     
-    #image = forms.ImageField(upload_to="photos/images")
+    image = forms.FileField()
     image_name= forms.CharField(max_length=100)
     image_desc =forms.CharField(max_length=1000)
-    
+    category= forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(attrs={
+        'palceholder' : 'Category'
+    }))

5c474aa8308d40d859f88ca08bf71ec4d2039500
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-13 10:33:23 +0300

    Creating category admin module to generate category slug similar to the category name

--- image/admin.py
+++ image/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Image
 # Register your models here.
-admin.site.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'image_slug' :('image_name',)}
+    list_display = ("image_name", "image_slug")
+
+admin.site.register(Image, ImageAdmin)

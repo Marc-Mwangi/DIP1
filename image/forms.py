fb47e9315d9dfb6dfd2552274cd9026cf2aa8807
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-03-23 12:23:56 +0300

    Upload functionaility

--- image/forms.py
+++ image/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from category.models import Category
+from .models import Image
 
-class ImageForm(forms.Form):
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('images','image_name', 'image_slug','image_desc','category')
+        
     
-    images = forms.FileField()
-    image_name= forms.CharField(max_length=100)
-    image_desc =forms.CharField(max_length=1000)
-    category= forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(attrs={
-        'palceholder' : 'Category'
-    }))
+    #images = forms.FileField()
+    #image_name= forms.CharField(max_length=100)
+    #image_desc =forms.CharField(max_length=1000)
+    #category= forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(attrs={
+    #    'palceholder' : 'Category'
+    #}))
--- image/views.py
+++ image/views.py
@@ -6,13 +6,20 @@ from image.forms import ImageForm
 # Create your views here.
 def images(request):
     
-    if request.method == "POST" and request.FILES['images']:
+    if request.method == "POST" :
         
         images = request.FILES.get("images")
         image_name = request.POST.get("image_name")
         image_desc = request.POST.get("image_desc")
         category = request.POST.get("category")
         
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            context={
+                    "form": form,
+                }
+            return render(request, 'index.html', context)
         #Get category associations
         image_category= Category.objects.get(id=category)
         
